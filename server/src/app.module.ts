e2757fd8c6710df6bf989a7fd58b44ac0558fe14
andreubltzr <andreubltzr11@gmail.com>
2023-08-17 21:24:26 +0200

    added project resource + db connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get<string>(
+          'DB_USER',
+        )}:${configService.get<string>(
+          'DB_PASSWORD',
+        )}@${configService.get<string>('DB_HOST')}/`,
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
